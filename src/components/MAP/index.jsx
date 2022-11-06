725ab5cf03d0c54be33c457bf6812495bcf7c1c8
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-11-06 19:00:49 +0100

    🚧 Work in progress : create multiple markers

--- src/components/MAP/index.jsx
+++ src/components/MAP/index.jsx
@@ -2,6 +2,30 @@ import React from "react";
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
+import L from "leaflet";
+import icon from "/images/icon-location.svg";
+
+let DefaultIcon = L.icon({
+        iconUrl: icon,
+        iconSize: [46, 56],
+});
+
+let arrCoordinates = [
+        [36.8065, 10.1815],
+        [36.8185, 10.1815],
+        [36.8065, 10.1915],
+];
+
+function MultipleMarkers() {
+        return arrCoordinates.map((coordinata, index) => {
+                return (
+                        <Marker
+                                key={index}
+                                position={coordinata}
+                                icon={DefaultIcon}></Marker>
+                );
+        });
+}
 
 const MAP = () => {
         return (
@@ -11,25 +35,15 @@ const MAP = () => {
                         scrollWheelZoom={false}>
                         <TileLayer
                                 attribution='&copy; <a href="https://github.com/MarwenLabidi">by MarwenLabidi</a> '
-                                url={`https://api.mapbox.com/styles/v1/abidimarwen/cla5hdzif000j14t37tq0lcc2/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAP_key}`}
+                                url={`https://api.mapbox.com/styles/v1/abidimarwen/cla5hdzif000j14t37tq0lcc2/tiles/256/{z}/{x}/{y}@2x?access_token=${
+                                        import.meta.env.VITE_MAP_key
+                                }`}
                         />
-                        <Marker position={[36.8065, 10.1815]}>
-                                <Popup>
-                                        A pretty CSS3 popup. <br /> Easily
-                                        customizable.
-                                </Popup>
-                        </Marker>
+                        <MultipleMarkers />
                 </MapContainer>
         );
 };
 
 export default MAP;
 
-//change marker icon
-import L from "leaflet";
-import icon from "/images/icon-location.svg";
-let DefaultIcon = L.icon({
-        iconUrl: icon,
-        iconSize: [46, 56],
-});
-L.Marker.prototype.options.icon = DefaultIcon;
+
