d6675350181d6d3da088144b166360380a6b4787
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-11-06 16:52:05 +0100

    🚧 Work in progress : i create env var for key map and change the attrbution

--- src/components/MAP/index.jsx
+++ src/components/MAP/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { MapContainer, TileLayer,Popup,Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "./index.css"
+import "./index.css";
 
 const MAP = () => {
         return (
@@ -10,8 +10,8 @@ const MAP = () => {
                         zoom={13}
                         scrollWheelZoom={false}>
                         <TileLayer
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                url='https://api.mapbox.com/styles/v1/abidimarwen/cla5hdzif000j14t37tq0lcc2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWJpZGltYXJ3ZW4iLCJhIjoiY2xhNWhqNGVwMGl1MTNwcWtnemY1ZWp6cCJ9.ECULKyYrTHQKtp_l4Z_WkA'
+                                attribution='&copy; <a href="https://github.com/MarwenLabidi">by MarwenLabidi</a> '
+                                url={`https://api.mapbox.com/styles/v1/abidimarwen/cla5hdzif000j14t37tq0lcc2/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAP_key}`}
                         />
                         <Marker position={[36.8065, 10.1815]}>
                                 <Popup>
@@ -30,6 +30,6 @@ import L from "leaflet";
 import icon from "/images/icon-location.svg";
 let DefaultIcon = L.icon({
         iconUrl: icon,
-	iconSize: [46, 56],
+        iconSize: [46, 56],
 });
 L.Marker.prototype.options.icon = DefaultIcon;
