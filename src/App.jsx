d72db8c478eb41a59cbd47411682e77656f4f4af
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-11-14 21:21:25 +0100

    ➕ Adding  : i added TODO tasks

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,10 @@ function App() {
 }
 
 export default App;
-
-//-[] create zustand store : showdetails :fale, coordinate :{lg,wg}, bounds : {ne: null, sw: null}
+//-[] create autocomple components(there is location icon and place name) 5 placess to choose from
+//-[] create zustand store : showdetails :fale, coordinate :{lg,wg}, bounds : {ne: null, sw: null} place : []
+//-[] fetch the places from the api [https://nominatim.openstreetmap.org/search?q=tunis&format=geojson] using custom hooks react query and set them to the place state or make her ref
 //-[] create th useRestaurant data ustom hooks uing react query to get restaurant data
 //-[] create geocoder with the input filed and us formik
 //-[] create the direction feature
+//-[] add rating starts from material ui 
