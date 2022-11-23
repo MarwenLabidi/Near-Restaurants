bf82472ec748bcb905a7f4dedf660c57a64a0b2c
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-11-23 14:03:56 +0100

    🚧 Work in progress : reastaurant store

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import DETAILS_CARD from "./components/DETAILS_CARD";
 import HEADER from "./components/HEADER";
 import MAP from "./components/MAP";
 import "./App.css";
 import useShowdetails from "./store/ShowCardDetailStore";
+import useCoordinateStore from "./store/coordinateStore";
+import { getTheCurrentLocation } from "./utils/utils";
 
 function App() {
         const { showdetails, toggleShowdetails } = useShowdetails((state) => ({
                 showdetails: state.showdetails,
                 toggleShowdetails: state.toggleShowdetails,
         }));
-        //TODO? useeffect to set the current courdinate
+        const { coordinate, bounds, setCoordinate, setBonds } =
+        useCoordinateStore((state) => ({
+                coordinate: state.coordinate,
+                bounds: state.bounds,
+                setCoordinate: state.setCoordinate,
+                setBonds: state.setBonds,
+        }));
+        // set the current location to the state
+        useEffect(() => {
+                getTheCurrentLocation(setCoordinate);
+        }, []);
         //TODO? useEffect to get the restaurant whatevent the cooridnate and boundsstate change
+        useEffect(() => {
+                console.log(coordinate);
+        }, [coordinate]);
 
         return (
                 <>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,7 +12,7 @@ export function debounce(cb, addPlace, delay = 1000) {
         };
 }
 
-export const pickPlace = (e,placeInTheInputRef,addPlace) => {
+export const pickPlace = (e, placeInTheInputRef, addPlace) => {
         let place = e.target.getAttribute("data-places");
         place = JSON.parse(place);
         // set the place you clicked to placeInTheInputRef
@@ -22,4 +22,43 @@ export const pickPlace = (e,placeInTheInputRef,addPlace) => {
                 place.display_name;
         //hide the autocomplete
         addPlace([]);
+};
+
+export const getTheCurrentLocation = async (setCoordinate) => {
+        if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                        showPosition,
+                        showError
+                );
+
+                function showPosition(position) {
+                        setCoordinate(position.coords.latitude, position.coords.longitude)
+                }
+                function showError(error) {
+                        switch (error.code) {
+                                case error.PERMISSION_DENIED:
+                                        console.error(
+                                                "User denied the request for Geolocation."
+                                        );
+                                        break;
+                                case error.POSITION_UNAVAILABLE:
+                                        console.error(
+                                                "Location information is unavailable."
+                                        );
+                                        break;
+                                case error.TIMEOUT:
+                                        console.error(
+                                                "The request to get user location timed out."
+                                        );
+                                        break;
+                                case error.UNKNOWN_ERROR:
+                                        console.error(
+                                                "An unknown error occurred."
+                                        );
+                                        break;
+                        }
+                }
+        } else {
+                alert("Geolocation is not supported by this browser.");
+        }
 };
